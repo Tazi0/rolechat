078124a037f817b9da32f774921c70948e511f19
rhys19 <30848481+rhys19@users.noreply.github.com>
2019-04-06 23:31:52 -0500

    Update server.lua
    fixed so it uses ace perms in master :D

--- rolechat/server.lua
+++ rolechat/server.lua
@@ -7,17 +7,17 @@
 AddEventHandler('chatMessage', function(source, Name, Msg)
     args = stringsplit(Msg, " ")
     CancelEvent()
-          if exports.discord_perms:IsRolePresent(user, Owner) then
+          if IsPlayerAceAllowed(source, Owner) then
               TriggerClientEvent('chat:addMessage', -1, { args = { "^7[ ^1Owner ^7] (^3 " .. Name.." ^7)", "^8"..Msg }, color = 255, 0, 0 })
-          elseif exports.discord_perms:IsRolePresent(user, Management) then
+          elseif IsPlayerAceAllowed(source, Management) then
               TriggerClientEvent('chat:addMessage', -1, { args = { "^7[ ^5Management ^7] (^3 " .. Name.." ^7)", Msg }, color = 255,255,255 })
-          elseif exports.discord_perms:IsRolePresent(user, Senior_Admin) then
+          elseif IsPlayerAceAllowed(source, Senior_Admin) then
               TriggerClientEvent('chat:addMessage', -1, { args = { "^7[ ^8Senior Admin ^7] (^3 " .. Name.." ^7)", Msg }, color = 255,255,255 })
-          elseif exports.discord_perms:IsRolePresent(user, Admin) then
+          elseif IsPlayerAceAllowed(source, Admin) then
               TriggerClientEvent('chat:addMessage', -1, { args = { "^7[ ^8Admin ^7] (^3 " .. Name.." ^7)", Msg }, color = 255,255,255 })
-	        elseif exports.discord_perms:IsRolePresent(user, Moderator) then
+	        elseif IsPlayerAceAllowed(source, Moderator) then
              TriggerClientEvent('chat:addMessage', -1, { args = { "^7[ ^8Moderator ^7] (^3 " .. Name.." ^7)", Msg }, color = 255,255,255 })
-	        elseif exports.discord_perms:IsRolePresent(user, Tmod) then
+	        elseif IsPlayerAceAllowed(source, Tmod) then
               TriggerClientEvent('chat:addMessage', -1, { args = { "^7[ ^8T-Mod ^7] (^3 " .. Name.." ^7)", Msg }, color = 255,255,255 })
           else
               TriggerClientEvent('chat:addMessage', -1, { args = { "^7[ ^4Member ^7] (^3 " .. Name.." ^7)", Msg }, color = 255,255,255 })
@@ -40,7 +40,7 @@ end
 -- version checker
 local CurrentVersion = '1.3'
 local GithubResourceName = 'roleschat'
-local githubacct = "Rhys19"
+local githubacct = "Tazi0"
 local resourceName = GetCurrentResourceName()
 local versionurl = "https://raw.githubusercontent.com/"..githubacct.."/"..GithubResourceName.."/master/VERSION"
 local changesurl = "https://raw.githubusercontent.com/"..githubacct.."/"..GithubResourceName.."/master/CHANGES"
